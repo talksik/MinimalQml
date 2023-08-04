baf658b4dbe432bbd714eda053711e0ccacbb930
talksik <patel.arjun50@gmail.com>
2023-08-04 16:22:59 -0700

    small mods

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ mkdir build &&
 cd build &&
 
 echo "Running cmake with build type Debug."
-cmake -DCMAKE_PREFIX_PATH=$HOME/Qt/5.15.14/gcc_64/ \
+cmake -Wall -DCMAKE_PREFIX_PATH=$HOME/Qt/5.15.14/gcc_64/ \
   -G "Unix Makefiles" \
   -DCMAKE_BUILD_TYPE=Debug .. &&
 
