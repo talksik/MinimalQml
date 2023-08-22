be3e7d6aedb03fb516db9dc1b9d037b2d8ce914f
talksik <patel.arjun50@gmai.com>
2023-08-22 14:24:45 -0700

    fixing commands

--- device.sh
+++ device.sh
@@ -2,10 +2,6 @@
 # this is special script for running on jetson nano
 
 export QT_DEBUG_PLUGINS=1
-export QT_QPA_EGLFS_WIDTH=1920
-export QT_QPA_EGLFS_HEIGHT=1080
-export QT_QPA_EGLFS_INTEGRATION=eglfs_kms
-
 export GST_DEBUG=2
 
 rm -rf build
@@ -22,6 +18,6 @@ make
 echo "running qml-example"
 
 cd ..
-./build/qml-example -platform eglfs
+./build/MinimalQml
 
 
